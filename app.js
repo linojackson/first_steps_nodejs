ae049464bec4e2d5e44ed37c4e1731135f07dfc7
Jackson Lino <jackson.lino@hotmail.com>
2022-03-21 15:02:21 -0300

    using modules

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-let testNode = true;
-const noChange = "jacksonlino.com.br";
+// let testNode = true;
+// const noChange = "jacksonlino.com.br";
 
-console.log("Hello World!");
-console.log("this time goes");
+// console.log("Hello World!");
+// console.log("this time goes");
 
-if (testNode) {
-    console.log(noChange);
-}
+// if (testNode) {
+//     console.log(noChange);
+// }
+var calculadora = require("./calculadora");
+
+console.log(calculadora.soma(30,12));
+console.log(calculadora.sub(30,12));
+console.log(calculadora.mult(30,12));
+console.log(calculadora.div(30,12));
